eba60749db2a82163466f72355be469c62eaaf94
hsl <32757468@qq.com>
2021-02-18 16:28:54 +0800

    初始化提交

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-essential",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+};
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Home />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Home from "@/views/Home/index.vue";
+
+export default defineComponent({
+  name: "App",
+  components: { Home },
+});
+</script>
+
+<style></style>
